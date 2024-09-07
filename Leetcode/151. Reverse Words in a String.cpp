cc70e9c12af23d908f5a56c2f5150cc59babfcd9
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-09-07 18:41:26 +0530

    update

---
+++ Leetcode/151. Reverse Words in a String.cpp
@@ -0,0 +1,37 @@
+// Iterate over the string and check.
+// If there are characters assigned them to temp and move forward.
+// If space is encountered then break from above loop.
+// Now check if temp size > 0
+// If ans size is 0 then assign temp to the ans.
+// Else do ans = temp+ " "+ans
+// If space is encountered just move the pointer forward.
+
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        int n = s.size();
+       int  i = 0;
+        string ans ="";
+
+        while(i<n){
+            string temp="";
+            while(s[i]==' ' && i<n){
+                i++;
+
+            }while(s[i]!=' ' && i<n){
+                temp +=s[i];
+                i++;
+            }
+            if(temp.size()>0){
+                if(ans.size()==0)
+                ans = temp;
+                else
+                ans = temp + " "+ans;
+            }
+        }
+
+        return ans;
+        
+    }
+};
